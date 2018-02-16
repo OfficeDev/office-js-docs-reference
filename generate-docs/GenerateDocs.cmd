3e6402be9a8b6bd57196e9954c0f31c8063b59ba
Kim Brandl <kibrandl@microsoft.com>
2018-02-15 17:50:46 -0800

    Rename folders and update GenerateDocs.cmd accordingly.

--- generate-docs/GenerateDocs.cmd
+++ generate-docs/GenerateDocs.cmd
@@ -2,14 +2,13 @@ call npm install
 
 del package-lock.json
 
-cd excel
+cd api-extractor-inputs
 
 call ..\node_modules\.bin\api-extractor run
 
 cd ..
 
-
-call .\node_modules\.bin\api-documenter yaml --input-folder .\json --office
+call .\node_modules\.bin\api-documenter yaml --input-folder .\api-documenter-inputs --office
 
 rem call D:\GitRepos\wbt2\libraries\api-documenter\ad.cmd yaml --input-folder .\json --office
 
