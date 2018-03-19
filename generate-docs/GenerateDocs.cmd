c157ad0ae690fce1ea799aa5dc49ecf28a941f16
Kim Brandl <kibrandl@microsoft.com>
2018-03-19 11:49:56 -0700

    Use separate d.ts file (and API Extractor process) for generating Excel docs.

--- generate-docs/GenerateDocs.cmd
+++ generate-docs/GenerateDocs.cmd
@@ -6,6 +6,10 @@ cd api-extractor-inputs
 
 call ..\node_modules\.bin\api-extractor run
 
+cd ..\api-extractor-inputs-excel
+
+call ..\node_modules\.bin\api-extractor run
+
 cd ..
 
 call .\node_modules\.bin\api-documenter yaml --input-folder .\json --office
