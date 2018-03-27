a35c2ac5cac1016714617aca6013723a3ec21b77
Kim Brandl <kibrandl@microsoft.com>
2018-03-27 14:15:59 -0700

    Add more logic to preprocessor script.

--- generate-docs/scripts/preprocessor.js
+++ generate-docs/scripts/preprocessor.js
@@ -14,24 +14,31 @@ const fsx = require("fs-extra");
 class DtsBuilder {
     constructor() {
         this.slashes = '////////////////////////////////////////////////////////////////';
+        // private makeHeader(text: string): string {
+        //     text = ' ' + text + ' ';
+        //     const textWithPrefix = this.slashes.substr(0, Math.floor((this.slashes.length - text.length) / 2)) + text;
+        //     return textWithPrefix + this.slashes.substr(0, this.slashes.length - textWithPrefix.length);
+        // }
     }
-    replaceDtsSection(definitions, beginMarker, endMarker, content) {
-        const definitionsLowercase = definitions.toLowerCase();
-        const indexOfBefore = this.indexOfOneAndOnlyOneLine(beginMarker.toLowerCase(), definitionsLowercase, "before");
-        const indexOfAfter = this.indexOfOneAndOnlyOneLine(endMarker.toLowerCase(), definitionsLowercase, "after");
-        const before = definitions.substring(0, indexOfBefore);
-        const after = definitions.substring(indexOfAfter);
-        return before +
-            '\n' + this.makeHeader(beginMarker) +
-            '\n' + this.slashes +
-            '\n' +
-            '\n' +
-            content +
-            '\n' +
-            '\n' + this.slashes +
-            '\n' + this.makeHeader(endMarker) +
-            '\n' + after;
-    }
+    // public replaceDtsSection(definitions: string, beginMarker: string, endMarker: string, content: string): string {
+    //     const definitionsLowercase = definitions.toLowerCase();
+    //     const indexOfBefore = this.indexOfOneAndOnlyOneLine(
+    //         beginMarker.toLowerCase(), definitionsLowercase, "before");
+    //     const indexOfAfter = this.indexOfOneAndOnlyOneLine(
+    //         endMarker.toLowerCase(), definitionsLowercase, "after");
+    //     const before = definitions.substring(0, indexOfBefore);
+    //     const after = definitions.substring(indexOfAfter);
+    //     return before +
+    //         '\n' + this.makeHeader(beginMarker) +
+    //         '\n' + this.slashes +
+    //         '\n' +
+    //         '\n' +
+    //         content +
+    //         '\n' +
+    //         '\n' + this.slashes +
+    //         '\n' + this.makeHeader(endMarker) +
+    //         '\n' + after;
+    // }
     extractDtsSection(definitions, beginMarker, endMarker) {
         const definitionsLowercase = definitions.toLowerCase();
         const indexOfBefore = this.indexOfOneAndOnlyOneLine(beginMarker.toLowerCase(), definitionsLowercase, "before");
@@ -59,11 +66,6 @@ class DtsBuilder {
                 throw new Error("Invalid position specified");
         }
     }
-    makeHeader(text) {
-        text = ' ' + text + ' ';
-        const textWithPrefix = this.slashes.substr(0, Math.floor((this.slashes.length - text.length) / 2)) + text;
-        return textWithPrefix + this.slashes.substr(0, this.slashes.length - textWithPrefix.length);
-    }
 }
 exports.DtsBuilder = DtsBuilder;
 (() => __awaiter(this, void 0, void 0, function* () {
@@ -80,9 +82,27 @@ exports.DtsBuilder = DtsBuilder;
         .replace(/^(\s*)(function)(\s+)/gm, `$1export $2$3`)
         .replace(/(\s*)(@param)(\s+)(\w+)(\s)(\s)/g, `$1$2$3$4$5`)
         .replace(/(\s*)(@param)(\s+)(\w+)(\s+)([^\-])/g, `$1$2$3$4$5- $6`);
-    // remove the OfficeCore section from the d.ts file
-    definitions = dtsBuilder.replaceDtsSection(definitions, "Begin OfficeCore", "End OfficeCore", "");
+    // // remove the OfficeCore section from the d.ts file
+    // definitions = dtsBuilder.replaceDtsSection(
+    //     definitions,
+    //     "Begin OfficeCore",
+    //     "End OfficeCore",
+    //     ""
+    // );
     // create file: excel.d.ts
-    fsx.writeFileSync('excel.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Excel APIs", "End Excel APIs"));
+    fsx.writeFileSync('../api-extractor-inputs-excel/excel.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Excel APIs", "End Excel APIs"));
+    // create file: office.d.ts
+    fsx.writeFileSync('../api-extractor-inputs-office/office.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Office namespace", "End Office namespace") +
+        '\n' +
+        '\n' +
+        dtsBuilder.extractDtsSection(definitions, "Begin OfficeExtension runtime", "End OfficeExtension runtime"));
+    // create file: onenote.d.ts
+    fsx.writeFileSync('../api-extractor-inputs-onenote/onenote.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin OneNote APIs", "End OneNote APIs"));
+    // create file: outlook.d.ts
+    fsx.writeFileSync('../api-extractor-inputs-outlook/outlook.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Exchange APIs", "End Exchange APIs"));
+    // create file: viso.d.ts
+    fsx.writeFileSync('../api-extractor-inputs-visio/visio.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Visio APIs", "End Visio APIs"));
+    // create file: word.d.ts
+    fsx.writeFileSync('../api-extractor-inputs-word/word.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Word APIs", "End Word APIs"));
 }))();
 //# sourceMappingURL=preprocessor.js.map
--- generate-docs/scripts/preprocessor.ts
+++ generate-docs/scripts/preprocessor.ts
@@ -6,28 +6,28 @@ import * as fsx from 'fs-extra';
 export class DtsBuilder {
     private slashes = '////////////////////////////////////////////////////////////////';
 
-    public replaceDtsSection(definitions: string, beginMarker: string, endMarker: string, content: string): string {
-        const definitionsLowercase = definitions.toLowerCase();
+    // public replaceDtsSection(definitions: string, beginMarker: string, endMarker: string, content: string): string {
+    //     const definitionsLowercase = definitions.toLowerCase();
 
-        const indexOfBefore = this.indexOfOneAndOnlyOneLine(
-            beginMarker.toLowerCase(), definitionsLowercase, "before");
-        const indexOfAfter = this.indexOfOneAndOnlyOneLine(
-            endMarker.toLowerCase(), definitionsLowercase, "after");
+    //     const indexOfBefore = this.indexOfOneAndOnlyOneLine(
+    //         beginMarker.toLowerCase(), definitionsLowercase, "before");
+    //     const indexOfAfter = this.indexOfOneAndOnlyOneLine(
+    //         endMarker.toLowerCase(), definitionsLowercase, "after");
 
-        const before = definitions.substring(0, indexOfBefore);
-        const after = definitions.substring(indexOfAfter);
+    //     const before = definitions.substring(0, indexOfBefore);
+    //     const after = definitions.substring(indexOfAfter);
 
-        return before +
-            '\n' + this.makeHeader(beginMarker) +
-            '\n' + this.slashes +
-            '\n' +
-            '\n' +
-            content +
-            '\n' +
-            '\n' + this.slashes +
-            '\n' + this.makeHeader(endMarker) +
-            '\n' + after;
-    }
+    //     return before +
+    //         '\n' + this.makeHeader(beginMarker) +
+    //         '\n' + this.slashes +
+    //         '\n' +
+    //         '\n' +
+    //         content +
+    //         '\n' +
+    //         '\n' + this.slashes +
+    //         '\n' + this.makeHeader(endMarker) +
+    //         '\n' + after;
+    // }
 
     public extractDtsSection(definitions: string, beginMarker: string, endMarker: string): string {
         const definitionsLowercase = definitions.toLowerCase();
@@ -63,11 +63,11 @@ export class DtsBuilder {
         }
     }
 
-    private makeHeader(text: string): string {
-        text = ' ' + text + ' ';
-        const textWithPrefix = this.slashes.substr(0, Math.floor((this.slashes.length - text.length) / 2)) + text;
-        return textWithPrefix + this.slashes.substr(0, this.slashes.length - textWithPrefix.length);
-    }
+    // private makeHeader(text: string): string {
+    //     text = ' ' + text + ' ';
+    //     const textWithPrefix = this.slashes.substr(0, Math.floor((this.slashes.length - text.length) / 2)) + text;
+    //     return textWithPrefix + this.slashes.substr(0, this.slashes.length - textWithPrefix.length);
+    // }
 }
 
 (async () => {
@@ -87,16 +87,52 @@ export class DtsBuilder {
         .replace(/(\s*)(@param)(\s+)(\w+)(\s)(\s)/g, `$1$2$3$4$5`)
         .replace(/(\s*)(@param)(\s+)(\w+)(\s+)([^\-])/g, `$1$2$3$4$5- $6`);
 
-    // remove the OfficeCore section from the d.ts file
-    definitions = dtsBuilder.replaceDtsSection(
-        definitions,
-        "Begin OfficeCore",
-        "End OfficeCore",
-        ""
-    );
+    // // remove the OfficeCore section from the d.ts file
+    // definitions = dtsBuilder.replaceDtsSection(
+    //     definitions,
+    //     "Begin OfficeCore",
+    //     "End OfficeCore",
+    //     ""
+    // );
 
     // create file: excel.d.ts
-    fsx.writeFileSync('excel.d.ts', dtsBuilder.extractDtsSection(definitions, "Begin Excel APIs", "End Excel APIs"));
+    fsx.writeFileSync(
+        '../api-extractor-inputs-excel/excel.d.ts', 
+        dtsBuilder.extractDtsSection(definitions, "Begin Excel APIs", "End Excel APIs")
+    );
+
+    // create file: office.d.ts
+    fsx.writeFileSync(
+        '../api-extractor-inputs-office/office.d.ts', 
+        dtsBuilder.extractDtsSection(definitions, "Begin Office namespace", "End Office namespace") + 
+        '\n' + 
+        '\n' +
+        dtsBuilder.extractDtsSection(definitions, "Begin OfficeExtension runtime", "End OfficeExtension runtime")
+    );
+
+    // create file: onenote.d.ts
+    fsx.writeFileSync(
+        '../api-extractor-inputs-onenote/onenote.d.ts', 
+        dtsBuilder.extractDtsSection(definitions, "Begin OneNote APIs", "End OneNote APIs")
+    );
+
+    // create file: outlook.d.ts
+    fsx.writeFileSync(
+        '../api-extractor-inputs-outlook/outlook.d.ts', 
+        dtsBuilder.extractDtsSection(definitions, "Begin Exchange APIs", "End Exchange APIs")
+    );
+
+    // create file: viso.d.ts
+    fsx.writeFileSync(
+        '../api-extractor-inputs-visio/visio.d.ts', 
+        dtsBuilder.extractDtsSection(definitions, "Begin Visio APIs", "End Visio APIs")
+    );
+
+    // create file: word.d.ts
+    fsx.writeFileSync(
+        '../api-extractor-inputs-word/word.d.ts', 
+        dtsBuilder.extractDtsSection(definitions, "Begin Word APIs", "End Word APIs")
+    );
 
 })();
 
