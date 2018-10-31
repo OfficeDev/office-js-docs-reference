a6351ac4703a83ec392bd3813685e334ba54046b
Alex Jerabek <aljerabe@microsoft.com>
2018-10-31 11:45:43 -0700

    What's New Generation Tool

---
+++ generate-docs/tools/WhatsNew.ts
@@ -0,0 +1,260 @@
+// usage: node WhatsNew "Old d.ts" "New d.ts" "link prefix"
+// example: node WhatsNew excel-1_7.d.ts excel-1_8.d.ts javascript/api/excel/excel.
+
+import { readFileSync } from "fs";
+import * as fsx from "fs-extra";
+import * as ts from "typescript";
+
+enum FieldType {
+    Property = "Property",
+    Method = "Method",
+    Event = "Event",
+    Enum = "Enum"
+}
+
+class FieldStruct {
+    declarationString: string;
+    comment: string;
+    type: FieldType;
+
+    constructor(decString: string, commentString: string, fieldType: FieldType) {
+        this.declarationString = decString;
+        this.comment = commentString;
+        this.type = fieldType;
+    }
+}
+
+class ClassStruct {
+    declarationString: string;
+    comment: string;
+    fields: FieldStruct[];
+
+    constructor(decString: string, commentString: string) {
+        this.declarationString = decString;
+        this.comment = commentString;
+        this.fields = [];
+   }
+
+   copyWithoutFields(): ClassStruct {
+       return new ClassStruct(this.declarationString, this.comment);
+   }
+
+   sortFields(): void {
+       this.fields.sort((a, b) => {
+           if (a.declarationString === b.declarationString) {
+               return 0;
+           } else {
+            return a.declarationString < b.declarationString ? -1 : 1;
+           }
+       });
+   }
+
+   getClassName(): string {
+       return this.declarationString.substring(this.declarationString.lastIndexOf(" ") + 1);
+   }
+}
+
+class APISet {
+    api: ClassStruct[];
+    constructor() {
+         this.api = [];
+    }
+
+    addClass(clas: ClassStruct): void {
+        this.api.push(clas);
+    }
+
+    containsClass(clas: ClassStruct): boolean {
+        let found: boolean = false;
+        this.api.forEach(element => {
+            if (element.declarationString === clas.declarationString) {
+                found = true;
+            }
+        });
+
+        return found;
+    }
+
+    containsField(field: FieldStruct): boolean {
+        let found: boolean = false;
+        this.api.forEach(element => {
+            element.fields.forEach(thisField => {
+                if (thisField.declarationString === field.declarationString) {
+                    found = true;
+                }
+            });
+        });
+
+        return found;
+    }
+
+    getParentClassShell(field: FieldStruct): ClassStruct {
+        let parent: ClassStruct = null;
+        this.api.forEach(element => {
+            element.fields.forEach(thisField => {
+                if (thisField.declarationString === field.declarationString) {
+                    parent = element.copyWithoutFields();
+                }
+            });
+        });
+        return parent;
+    }
+
+    diff(other: APISet): APISet {
+        let diffAPI: APISet = new APISet();
+        this.api.forEach(element => {
+            if (!other.containsClass(element)) {
+                diffAPI.addClass(element);
+            } else {
+                let classShell: ClassStruct = null;
+                element.fields.forEach(field => {
+                    if (!other.containsField(field)) {
+                        if (classShell === null) {
+                            classShell = this.getParentClassShell(field);
+                            diffAPI.addClass(classShell);
+                        }
+                        classShell.fields.push(field);
+                    }
+                });
+            }
+        });
+
+        return diffAPI;
+    }
+
+    getAsDTS(): string {
+        this.sort();
+        let output: string = "";
+        this.api.forEach(clas => {
+            output += clas.comment.trim() + "\n";
+            output += clas.declarationString + " {\n";
+            clas.fields.forEach(field => {
+                output += "    " + field.comment + "\n";
+                output += "    " + field.declarationString + "\n";
+            });
+            output += "}\n";
+        });
+        return output;
+    }
+
+    getAsMarkdown(folder: string): string {
+        this.sort();
+        let output: string = "|Object|What's new|Description|\n";
+        output += "|:----|:----|:----|\n";
+        this.api.forEach(clas => {
+            clas.fields.forEach(field => {
+                let className: string = clas.getClassName();
+                let tableLine: string = "|[" + className + "](/" + relativePath + className.toLowerCase() + ")|";
+                let newItemText: string = field.declarationString.replace(/;/g, "");
+                switch (field.type) {
+                    case FieldType.Property:
+                    newItemText = newItemText.substring(0, newItemText.indexOf(":")).replace("readonly ", "");
+                    break;
+                    case FieldType.Method:
+                    newItemText = newItemText.substring(0, newItemText.indexOf(" =>"));
+                    break;
+                    case FieldType.Enum:
+                    newItemText = newItemText.substring(0, newItemText.indexOf(" ="));
+                    break;
+                }
+                let firstSentenceIndex: number = field.comment.indexOf("* ") + 2;
+                tableLine += newItemText + "|";
+                tableLine += field.comment.substring(firstSentenceIndex, field.comment.indexOf("\n", firstSentenceIndex)) + "|";
+                output += tableLine + "\n";
+            });
+        });
+        return output;
+    }
+
+    sort(): void {
+        this.api.forEach(element => {
+            element.sortFields();
+        });
+
+        this.api.sort((a, b) => {
+            if (a.getClassName() === b.getClassName()) {
+                return 0;
+            } else {
+             return a.getClassName() < b.getClassName() ? -1 : 1;
+            }
+        });
+    }
+}
+
+let releaseAPI: APISet = new APISet();
+let betaAPI: APISet = new APISet();
+
+// read files
+const fileNames: string[] = process.argv.slice(2);
+let releaseFile: ts.SourceFile = ts.createSourceFile(
+    fileNames[0],
+    readFileSync(fileNames[0]).toString(),
+    ts.ScriptTarget.ES2015,
+    true
+);
+let betaFile: ts.SourceFile = ts.createSourceFile(
+    fileNames[1],
+    readFileSync(fileNames[1]).toString(),
+    ts.ScriptTarget.ES2015,
+    true
+);
+
+let lastItem: ClassStruct | FieldStruct = null;
+
+parseDTS(releaseFile, releaseAPI, null);
+parseDTS(betaFile, betaAPI, null);
+
+let diffAPI : APISet = betaAPI.diff(releaseAPI);
+
+let relativePath: string = process.argv[4];
+fsx.writeFileSync("WhatsNew.d.ts", diffAPI.getAsDTS());
+fsx.writeFileSync("WhatsNew.md", diffAPI.getAsMarkdown(relativePath));
+
+function parseDTS(node: ts.Node, allClasses: APISet, topClass: ClassStruct): void {
+    switch (node.kind) {
+        case ts.SyntaxKind.InterfaceDeclaration:
+        let interfaceDeclaration : ts.InterfaceDeclaration = <ts.InterfaceDeclaration>node;
+        topClass = new ClassStruct("export interface " + interfaceDeclaration.name.text, "");
+        allClasses.addClass(topClass);
+        lastItem = topClass;
+        break;
+        case ts.SyntaxKind.ClassDeclaration:
+        let classDeclaration : ts.ClassDeclaration = <ts.ClassDeclaration>node;
+        topClass = new ClassStruct("export class " + classDeclaration.name.text, "");
+        allClasses.addClass(topClass);
+        lastItem = topClass;
+        break;
+        case ts.SyntaxKind.EnumDeclaration:
+        let enumDeclaration : ts.EnumDeclaration = <ts.EnumDeclaration>node;
+        topClass = new ClassStruct("export enum " + enumDeclaration.name.text, "");
+        allClasses.addClass(topClass);
+        lastItem = topClass;
+        break;
+        case ts.SyntaxKind.PropertyDeclaration:
+        let propSignature : ts.PropertyDeclaration = <ts.PropertyDeclaration>node;
+        let newProp: FieldStruct = new FieldStruct(propSignature.getText(), "", FieldType.Property);
+        topClass.fields.push(newProp);
+        lastItem = newProp;
+        break;
+        case ts.SyntaxKind.EnumMember:
+        let enumSignature: ts.EnumMember = <ts.EnumMember>node;
+        let newEnum: FieldStruct = new FieldStruct(enumSignature.getText(), "", FieldType.Enum);
+        topClass.fields.push(newEnum);
+        lastItem = newEnum;
+        break;
+        case ts.SyntaxKind.MethodSignature:
+        let methodSignature : ts.MethodSignature = <ts.MethodSignature>node;
+        let newMethod: FieldStruct = new FieldStruct(methodSignature.getText(), "", FieldType.Method);
+        topClass.fields.push(newMethod);
+        lastItem = newMethod;
+        break;
+        default:
+        if (node.getText().indexOf("/**") >= 0 && node.getText().indexOf("*/") && lastItem !== null && lastItem.comment === "") {
+            lastItem.comment = node.getText().replace(/    \*/g, "*");
+        }
+    }
+    node.getChildren().forEach(element => {
+        parseDTS(element, allClasses, topClass);
+    });
+}
+
