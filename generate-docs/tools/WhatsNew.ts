d76fc84917e9efb15394ebb58092f9934766494e
Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
2019-01-04 12:17:08 -0800

    Removing question mark characters from optional parameters (#280)
    * Removing question mark characters from optional parameters
    
    * Stripping optional character from properties only

--- generate-docs/tools/WhatsNew.ts
+++ generate-docs/tools/WhatsNew.ts
@@ -165,6 +165,10 @@ class APISet {
                     let newItemText = field.declarationString.replace(/;/g, "");
                     newItemText = newItemText.substring(0, newItemText.lastIndexOf(":")).replace("readonly ", "");
                     newItemText = newItemText.replace(/\|/g, "\\|");
+                    if (field.type === FieldType.Property) {
+                        newItemText = newItemText.replace("?", "");
+                    }
+
                     let tableLine = "[" + newItemText + "]("
                         + buildFieldLink(relativePath, className, field) + ")|";
                     tableLine += extractFirstSentenceFromComment(field.comment);
@@ -278,7 +282,7 @@ function parseDTS(node: ts.Node, allClasses: APISet): void {
                 // clean up spacing as best we can for the diffed d.ts
                 lastItem.comment = node.getText().replace(/    \*/g, "*");
                 if (lastItem.comment.indexOf("@eventproperty") >= 0) {
-                    // events are indistingushable from properties aside from this tag
+                    // events are indistinguishable from properties aside from this tag
                     lastItem.type = FieldType.Event;
                 }
             }
@@ -311,7 +315,7 @@ function parseDTSFieldItem(
     }
 }
 
-// capturing these because of eccentrities with the compiler ordering
+// capturing these because of eccentricities with the compiler ordering
 let topClass: ClassStruct = null;
 let lastItem: ClassStruct | FieldStruct = null;
 
