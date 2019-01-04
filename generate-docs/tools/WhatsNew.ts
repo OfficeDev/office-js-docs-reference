50d241dc0538362c66f2e5911a159a0f221d2288
Alex Jerabek <aljerabe@microsoft.com>
2019-01-04 13:27:44 -0800

    Streamlining WhatsNew experience

--- generate-docs/tools/WhatsNew.ts
+++ generate-docs/tools/WhatsNew.ts
@@ -1,7 +1,6 @@
-// usage: node WhatsNew "Old d.ts" "New d.ts" "link prefix"
-// example: node WhatsNew excel-1_7.d.ts excel-1_8.d.ts javascript/api/excel/excel.
-
 import { readFileSync } from "fs";
+import { promptFromList } from '../scripts/simple-prompts';
+import { fetchAndThrowOnError, DtsBuilder} from '../scripts/util';
 import * as fsx from "fs-extra";
 import * as ts from "typescript";
 
@@ -319,33 +318,88 @@ function parseDTSFieldItem(
 let topClass: ClassStruct = null;
 let lastItem: ClassStruct | FieldStruct = null;
 
-(() => {
+tryCatch(async () => {
+    // Get file locations
+    const officeJSUrl = await promptFromList({
+        message: "Which d.ts file should be used as the RELEASE version?",
+        choices: [
+            { name: "DefinitelyTyped", value: "https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/office-js/index.d.ts" },
+            { name: "Local file [generate-docs\\tools\\tool-inputs\\release.d.ts]", value: "" }
+        ]
+    });
+
+    if (officeJSUrl.length > 0) {
+        fsx.writeFileSync("./tool-inputs/release.d.ts", await fetchAndThrowOnError(officeJSUrl, "text"));
+    }
+
+    const officeJSPreviewUrl = await promptFromList({
+        message: "Which d.ts file should be used as the PREVIEW version?",
+        choices: [
+            { name: "DefinitelyTyped", value: "https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/office-js-preview/index.d.ts" },
+            { name: "Local file [generate-docs\\tools\\tool-inputs\\preview.d.ts]", value: "" }
+        ]
+    });
+
+    if (officeJSPreviewUrl.length > 0) {
+        fsx.writeFileSync("./tool-inputs/preview.d.ts", await fetchAndThrowOnError(officeJSPreviewUrl, "text"));
+    }
+
+    // read whole files
+    let wholeRelease = fsx.readFileSync("./tool-inputs/release.d.ts").toString();
+    let wholePreview = fsx.readFileSync("./tool-inputs/preview.d.ts").toString();
+
+    const hostName = await promptFromList({
+        message: "Which host is being generated?",
+        choices: [
+            { name: "Excel", value: "excel" },
+            { name: "OneNote", value: "onenote" },
+            { name: "Outlook", value: "outlook" },
+            { name: "Visio", value: "visio" },
+            { name: "Word", value: "word" }
+        ]
+    });
+    const releaseHostFileName: string = './tool-inputs/' + hostName + '-release.d.ts';
+    const previewHostFileName: string = './tool-inputs/' + hostName + '-preview.d.ts';
+
+    const dtsBuilder = new DtsBuilder();
+    fsx.writeFileSync(
+        './tool-inputs/' + hostName + '-release.d.ts',
+        dtsBuilder.extractDtsSection(wholeRelease, "Begin Excel APIs", "End Excel APIs")
+    );
+    fsx.writeFileSync(
+        './tool-inputs/' + hostName + '-preview.d.ts',
+        dtsBuilder.extractDtsSection(wholePreview, "Begin Excel APIs", "End Excel APIs")
+    );
 
     const releaseAPI: APISet = new APISet();
-    const betaAPI: APISet = new APISet();
+    const previewAPI: APISet = new APISet();
 
-    // read files
-    const fileNames: string[] = process.argv.slice(2);
     const releaseFile: ts.SourceFile = ts.createSourceFile(
         "Release",
-        fixDTS(readFileSync(fileNames[0]).toString()),
+        fixDTS(readFileSync(releaseHostFileName).toString()),
         ts.ScriptTarget.ES2015,
         true);
-    const betaFile: ts.SourceFile = ts.createSourceFile(
+    const previewFile: ts.SourceFile = ts.createSourceFile(
         "Preview",
-        fixDTS(readFileSync(fileNames[1]).toString()),
+        fixDTS(readFileSync(previewHostFileName).toString()),
         ts.ScriptTarget.ES2015,
         true);
+
     parseDTS(releaseFile, releaseAPI);
-    parseDTS(betaFile, betaAPI);
+    parseDTS(previewFile, previewAPI);
 
-    fsx.writeFileSync("Release.d.ts", releaseAPI.getAsDTS());
-    fsx.writeFileSync("Beta.d.ts", betaAPI.getAsDTS());
+    const diffAPI: APISet = previewAPI.diff(releaseAPI);
 
+    const relativePath: string = "javascript/api/" + hostName + "/" + hostName + ".";
+    fsx.writeFileSync("./tool-outputs/WhatsNew.d.ts", diffAPI.getAsDTS());
+    fsx.writeFileSync("./tool-outputs/WhatsNew.md", diffAPI.getAsMarkdown(relativePath));
+});
 
-    const diffAPI: APISet = betaAPI.diff(releaseAPI);
-
-    const relativePath: string = process.argv[4];
-    fsx.writeFileSync("WhatsNew.d.ts", diffAPI.getAsDTS());
-    fsx.writeFileSync("WhatsNew.md", diffAPI.getAsMarkdown(relativePath));
-})();
+async function tryCatch(call: () => Promise<void>) {
+    try {
+        await call();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
