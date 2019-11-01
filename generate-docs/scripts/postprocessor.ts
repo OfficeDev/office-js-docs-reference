3c1d8af528c4d6a040edcba4c968cdd98f3b326f
Alex Jerabek <aljerabe@microsoft.com>
2019-11-01 13:38:28 -0700

    Addressing UID

--- generate-docs/scripts/postprocessor.ts
+++ generate-docs/scripts/postprocessor.ts
@@ -207,10 +207,10 @@ function fixToc(tocPath: string, commonToc: INewToc, versionNumber: number): INe
 
     let excelIconSetFilter : string [] = ["FiveArrowsGraySet", "FiveArrowsSet", "FiveBoxesSet", "FiveQuartersSet", "FiveRatingSet", "FourArrowsGraySet", "FourArrowsSet", "FourRatingSet", "FourRedToBlackSet", "FourTrafficLightsSet", "IconCollections", "ThreeArrowsGraySet", "ThreeArrowsSet", "ThreeFlagsSet",  "ThreeSignsSet", "ThreeStarsSet",  "ThreeSymbols2Set", "ThreeSymbolsSet", "ThreeTrafficLights1Set", "ThreeTrafficLights2Set", "ThreeTrianglesSet"];
 
-    let customFunctionsRoot = {"name": "Custom Functions", "uid": "cfRoot", "items": [] as any};
+    let customFunctionsRoot = {"name": "Custom Functions", "uid": "", "items": [] as any};
 
     // create folders for OneNote subcategories
-    let oneNoteEnumRoot = {"name": "Enums", "uid": "onenoteEnums", "items": [] as any};
+    let oneNoteEnumRoot = {"name": "Enums", "uid": "", "items": [] as any};
     let oneNoteEnumFilter = generateEnumList(fsx.readFileSync("../api-extractor-inputs-onenote/onenote.d.ts").toString());
 
     // create folders for word subcategories
@@ -281,8 +281,8 @@ function fixToc(tocPath: string, commonToc: INewToc, versionNumber: number): INe
                             return excelFilter.indexOf(item.name) < 0;
                         });
 
-                        let excelEnumRoot = {"name": "Enums", "uid": "excelEnumRoot", "items": enumList};
-                        let excelIconSetRoot = {"name": "Icon Sets", "uid": "excelIconRoot", "items": iconSetList};
+                        let excelEnumRoot = {"name": "Enums", "uid": "", "items": enumList};
+                        let excelIconSetRoot = {"name": "Icon Sets", "uid": "", "items": iconSetList};
                         primaryList.unshift(excelIconSetRoot);
                         primaryList.unshift(excelEnumRoot);
                         if (versionNumber >= OLDEST_EXCEL_RELEASE_WITH_CUSTOM_FUNCTIONS) {
@@ -301,7 +301,7 @@ function fixToc(tocPath: string, commonToc: INewToc, versionNumber: number): INe
                             return wordFilter.indexOf(item.name) < 0;
                         });
 
-                        let wordEnumRoot = {"name": "Enums", "uid": "wordEnumRoot", "items": enumList};
+                        let wordEnumRoot = {"name": "Enums", "uid": "", "items": enumList};
                         primaryList.unshift(wordEnumRoot);
                         newToc.items[0].items.push({
                             "name": packageName,
@@ -386,7 +386,7 @@ function fixCommonToc(tocPath: string): INewToc {
     }];
 
     // create folders for common (shared) API subcategories
-    let sharedEnumRoot = {"name": "Enums", "uid": "officeEnumRoot", "items": [] as any};
+    let sharedEnumRoot = {"name": "Enums", "uid": "", "items": [] as any};
     let sharedEnumFilter = generateEnumList(fsx.readFileSync("../api-extractor-inputs-office/office.d.ts").toString());
     let commonAPIRoot;
     let officeRuntimeRoot;
