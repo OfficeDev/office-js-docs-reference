d87ba2e3632abcd9dd13bad7ecc281fe44800b57
Elizabeth Samuel <elizs@microsoft.com>
2020-09-10 13:12:48 -0700

    [Admin] Get latest changes (#782)

--- generate-docs/api-extractor-inputs-office/office.d.ts
+++ generate-docs/api-extractor-inputs-office/office.d.ts
@@ -581,7 +581,7 @@ export declare namespace Office {
         /**
          * Specifies the controls in the tab, such as menu items, buttons, etc.
          */
-        controls?: Control[];
+        controls: Control[];
     }
     /**
      * Represents an individual control or command and the state it should have.
