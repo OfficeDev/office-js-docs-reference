fbc24ad6eeb52982a43b93f91c11f4d1b3bc148b
AlexJerabek <aljerabe@microsoft.com>
2021-04-30 14:35:14 -0700

    Take away preserve view

--- generate-docs/scripts/postprocessor.ts
+++ generate-docs/scripts/postprocessor.ts
@@ -62,7 +62,7 @@ tryCatch(async () => {
                                 {"name": "OneNote", "href": "/javascript/api/onenote?view=onenote-js-1.1"},
                                 {"name": "Outlook", "href": "/javascript/api/outlook?view=outlook-js-preview"},
                                 {"name": "PowerPoint", "href": "/javascript/api/powerpoint?view=powerpoint-js-1.1"},
-                                {"name": "Visio", "href": "/javascript/api/visio?view=visio-js-1.1"},
+                                {"name": "Visio", "href": "/javascript/api/visio?view=visio-js-1.1&"},
                                 {"name": "Word", "href": "/javascript/api/word?view=word-js-preview"},
                                 {"name": "Common APIs", "href": "/javascript/api/office?view=common-js"}] as any;
     fsx.writeFileSync(docsDestination + "/toc.yml", jsyaml.safeDump(globalToc));
