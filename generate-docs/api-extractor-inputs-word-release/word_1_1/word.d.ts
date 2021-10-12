665d520fa6d12b7cd560073d8eb97827b0c94b1b
Elizabeth Samuel <elizs@microsoft.com>
2021-10-12 11:03:45 -0700

    [all hosts] Get latest updates (#1061)

--- generate-docs/api-extractor-inputs-word-release/word_1_1/word.d.ts
+++ generate-docs/api-extractor-inputs-word-release/word_1_1/word.d.ts
@@ -1994,7 +1994,7 @@ export declare namespace Word {
     }
     /**
      * Specifies the options to be included in a search operation.
-     * 
+     *
      * To learn more about how to use search options in the Word JavaScript APIs, read {@link https://docs.microsoft.com/office/dev/add-ins/word/search-option-guidance | Use search options to find text in your Word add-in}.
      *
      * @remarks
@@ -5695,13 +5695,6 @@ export declare namespace Word {
             */
             body?: Word.Interfaces.BodyLoadOptions;
             
-            /**
-             *
-             * Gets or sets a value that indicates that, when opening a new document, whether it is allowed to close this document even if this document is untitled. True to close, false otherwise.
-             *
-             * [Api set: WordApi]
-             */
-            allowCloseOnUntitled?: boolean;
             /**
              * Indicates whether the changes in the document have been saved. A value of true indicates that the document hasn't changed since it was saved. Read-only.
              *
