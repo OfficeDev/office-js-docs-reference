cef04348a4b8a5ff8062f070f78b86a181d44917
Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
2022-01-27 13:09:04 -0800

    Removing unneeded regex replacements (#1142)

--- generate-docs/scripts/preprocessor.ts
+++ generate-docs/scripts/preprocessor.ts
@@ -229,16 +229,7 @@ function cleanUpDts(localDtsPath: string): string {
     let definitions = fsx.readFileSync(localDtsPath).toString();
 
     console.log("\nFixing issues with d.ts file...");
-    return applyRegularExpressions(
-        definitions
-        .replace(/([ ]*)load\(option\?: string \| string\[\]\): (Excel|Word|OneNote|Visio|PowerPoint)\.(.*);/g,
-                 "$1/**\n$1 * Queues up a command to load the specified properties of the object. You must call `context.sync()` before reading the properties.\n$1 * @param propertyNames - A comma-delimited string or an array of strings that specify the properties to load.\n$1 */\n$1load(propertyNames?: string | string[]): $2.$3;")
-        .replace(/([ ]*)load\(option\?: {\n[ ]*select\?: string;\n[ ]*expand\?: string;\n[ ]*}\): (Excel|Word|OneNote|Visio|PowerPoint)\.(.*);/gm,
-                 "$1/**\n$1 * Queues up a command to load the specified properties of the object. You must call `context.sync()` before reading the properties.\n$1 * @param propertyNamesAndPaths - Where propertyNamesAndPaths.select is a comma-delimited string that specifies the properties to load, and propertyNamesAndPaths.expand is a comma-delimited string that specifies the navigation properties to load.\n$1 */\n$1load(propertyNamesAndPaths?: { select?: string; expand?: string; }): $2.$3;")
-        .replace(/([ ]*)load\(option\?: (Excel|Word|OneNote|Visio|PowerPoint)\.Interfaces\.(.*)CollectionLoadOptions & [Excel|Word|OneNote|Visio|PowerPoint]\.Interfaces\.CollectionLoadOptions\): [Excel|Word|OneNote|Visio|PowerPoint]\.[.*]Collection;/g,
-                 "$1/**\n$1 * Queues up a command to load the specified properties of the object. You must call `context.sync()` before reading the properties.\n$1 * @param collectionLoadOptions - Where collectionLoadOptions.select is a comma-delimited string that specifies the properties to load, and collectionLoadOptions.expand is a comma-delimited string that specifies the navigation properties to load. collectionLoadOptions.top specifies the maximum number of collection items that can be included in the result. collectionLoadOptions.skip specifies the number of items that are to be skipped and not included in the result. If collectionLoadOptions.top is specified, the result set will start after skipping the specified number of items.\n$1 */\n$1load(collectionLoadOptions?: $2.Interfaces.$3CollectionLoadOptions & $2.Interfaces.CollectionLoadOptions): $2.$3Collection;")
-        .replace(/(extends OfficeCore.RequestContext)/g, `extends OfficeExtension.ClientRequestContext`)
-        );
+    return applyRegularExpressions(definitions.replace(/(extends OfficeCore.RequestContext)/g, `extends OfficeExtension.ClientRequestContext`));
 }
 
 
