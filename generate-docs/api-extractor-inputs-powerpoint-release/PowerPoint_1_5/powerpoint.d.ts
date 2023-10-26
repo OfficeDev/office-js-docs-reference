daee89dd39c97bc205456d93b5b732e1a8457a8e
Sam Ramon <15154970+samantharamon@users.noreply.github.com>
2023-10-26 15:59:40 -0700

    [All hosts] Get latest (#1723)

--- generate-docs/api-extractor-inputs-powerpoint-release/PowerPoint_1_5/powerpoint.d.ts
+++ generate-docs/api-extractor-inputs-powerpoint-release/PowerPoint_1_5/powerpoint.d.ts
@@ -1721,7 +1721,7 @@ export declare namespace PowerPoint {
         getItemAt(index: number): PowerPoint.SlideLayout;
         /**
          * Gets a layout using its unique ID.  If such a layout does not exist, an object with an `isNullObject` property set to true is returned. For further information,
-                    see {@link https://docs.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods and properties}.
+                    see {@link https://learn.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods and properties}.
          *
          * @remarks
          * [Api set: PowerPointApi 1.3]
@@ -3160,7 +3160,7 @@ export declare namespace PowerPoint {
         getItemAt(index: number): PowerPoint.Slide;
         /**
          * Gets a slide using its unique ID. If such a slide does not exist, an object with an `isNullObject` property set to true is returned. For further information,
-                    see {@link https://docs.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods
+                    see {@link https://learn.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods
                     and properties}.
          *
          * @remarks
@@ -3235,7 +3235,7 @@ export declare namespace PowerPoint {
         getItemAt(index: number): PowerPoint.Slide;
         /**
          * Gets a slide using its unique ID. If such a slide does not exist, an object with an `isNullObject` property set to true is returned. For further information,
-                    see {@link https://docs.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods
+                    see {@link https://learn.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods
                     and properties}.
          *
          * @remarks
@@ -3310,7 +3310,7 @@ export declare namespace PowerPoint {
         getItemAt(index: number): PowerPoint.SlideMaster;
         /**
          * Gets a Slide Master using its unique ID. If such a Slide Master does not exist, an object with an `isNullObject` property set to true is returned.
-                    For further information, see {@link https://docs.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods and properties}."
+                    For further information, see {@link https://learn.microsoft.com/office/dev/add-ins/develop/application-specific-api-model#ornullobject-methods-and-properties | *OrNullObject methods and properties}."
          *
          * @remarks
          * [Api set: PowerPointApi 1.3]
