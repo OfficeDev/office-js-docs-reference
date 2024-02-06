0bf92fa90fba8962037ce6a3b347ef3fab050f08
Elizabeth Samuel <elizs@microsoft.com>
2024-02-06 15:55:52 -0800

    [all hosts] Get latest

--- generate-docs/api-extractor-inputs-excel-release/Excel_1_10/excel.d.ts
+++ generate-docs/api-extractor-inputs-excel-release/Excel_1_10/excel.d.ts
@@ -3689,7 +3689,7 @@ export declare namespace Excel {
          * @param skipBlanks - True if to skip blank cells in the source range. Default is false.
          * @param transpose - True if to transpose the cells in the destination range. Default is false.
          */
-        copyFrom(sourceRange: Range | RangeAreas | string, copyTypeString?: "All" | "Formulas" | "Values" | "Formats", skipBlanks?: boolean, transpose?: boolean): void;
+        copyFrom(sourceRange: Range | RangeAreas | string, copyTypeString?: "All" | "Formulas" | "Values" | "Formats" | "Link", skipBlanks?: boolean, transpose?: boolean): void;
         /**
          * Deletes the cells associated with the range.
          *
@@ -4479,7 +4479,7 @@ export declare namespace Excel {
          * @param skipBlanks - True if to skip blank cells in the source range or `RangeAreas`. Default is false.
          * @param transpose - True if to transpose the cells in the destination `RangeAreas`. Default is false.
          */
-        copyFrom(sourceRange: Range | RangeAreas | string, copyTypeString?: "All" | "Formulas" | "Values" | "Formats", skipBlanks?: boolean, transpose?: boolean): void;
+        copyFrom(sourceRange: Range | RangeAreas | string, copyTypeString?: "All" | "Formulas" | "Values" | "Formats" | "Link", skipBlanks?: boolean, transpose?: boolean): void;
         /**
          * Returns a `RangeAreas` object that represents the entire columns of the `RangeAreas` (for example, if the current `RangeAreas` represents cells "B4:E11, H2", it returns a `RangeAreas` that represents columns "B:E, H:H").
          *
@@ -24087,6 +24087,8 @@ export declare namespace Excel {
         textAsNumber = "TextAsNumber"
     }
     /**
+     * Represents the ordering method to be used when sorting Chinese characters.
+     *
      * @remarks
      * [Api set: ExcelApi 1.2]
      */
@@ -25462,8 +25464,8 @@ export declare namespace Excel {
          * @remarks
          * [Api set: ExcelApi 1.9]
          */
-        formats = "Formats"
-    }
+        formats = "Formats",
+            }
     /**
      * @remarks
      * [Api set: ExcelApi 1.9]
