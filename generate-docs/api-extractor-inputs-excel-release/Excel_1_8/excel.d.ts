3ff82218a0b766869f0884bff4fb6366923c696f
Elizabeth Samuel <elizs@microsoft.com>
2024-11-07 09:15:31 -0800

    [all hosts] Get latest

--- generate-docs/api-extractor-inputs-excel-release/Excel_1_8/excel.d.ts
+++ generate-docs/api-extractor-inputs-excel-release/Excel_1_8/excel.d.ts
@@ -2458,7 +2458,7 @@ export declare namespace Excel {
         
         
         /**
-         * Renders the range as a Base64-encoded png image.
+         * Renders the range as a Base64-encoded PNG image.
                     **Important**: This API is currently unsupported in Excel for Mac. Visit {@link https://github.com/OfficeDev/office-js/issues/235 | OfficeDev/office-js Issue #235} for the current status.
          *
          * @remarks
@@ -11124,7 +11124,7 @@ export declare namespace Excel {
          */
         category: string;
         /**
-         * The comment field in the metadata of the workbook. These have no connection to comments by users made in the workbork.
+         * The Comments field in the metadata of the workbook. These have no connection to comments by users made in the workbook.
          *
          * @remarks
          * [Api set: ExcelApi 1.7]
@@ -23654,7 +23654,7 @@ export declare namespace Excel {
              */
             category?: string;
             /**
-             * The comment field in the metadata of the workbook. These have no connection to comments by users made in the workbork.
+             * The Comments field in the metadata of the workbook. These have no connection to comments by users made in the workbook.
              *
              * @remarks
              * [Api set: ExcelApi 1.7]
@@ -28095,7 +28095,7 @@ export declare namespace Excel {
              */
             category?: string;
             /**
-             * The comment field in the metadata of the workbook. These have no connection to comments by users made in the workbork.
+             * The Comments field in the metadata of the workbook. These have no connection to comments by users made in the workbook.
              *
              * @remarks
              * [Api set: ExcelApi 1.7]
@@ -34408,7 +34408,7 @@ export declare namespace Excel {
              */
             category?: boolean;
             /**
-             * The comment field in the metadata of the workbook. These have no connection to comments by users made in the workbork.
+             * The Comments field in the metadata of the workbook. These have no connection to comments by users made in the workbook.
              *
              * @remarks
              * [Api set: ExcelApi 1.7]
