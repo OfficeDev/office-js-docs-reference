17fc95d962fa8d3735920196d40dcc2ed2dd05ff
Alex Jerabek <38896772+AlexJerabek@users.noreply.github.com>
2025-01-02 16:15:51 -0800

    Update reference-coverage-tester

--- generate-docs/scripts/postprocessor.ts
+++ generate-docs/scripts/postprocessor.ts
@@ -176,14 +176,14 @@ tryCatch(async () => {
                             
                             fsx.writeFileSync(packageFolder + '/' + packageFileName, fsx.readFileSync(packageFolder + '/' + packageFileName).toString()
                                 .replace(/^\s*example: \[\]\s*$/gm, "") // Remove example field from yml as the OPS schema does not support it.
-                                .replace(/description: \\\*\n/g, "description:") // Remove descriptions that are just "\*".
+                                .replace(/description: \\\*[\r\n]/gm, "description:") // Remove descriptions that are just "\*".
                                 .replace(/\\\*/gm, "*")); // Fix asterisk protection.
                         });
                     } else if (subfilename.indexOf(".yml") > 0) {
                         
                         fsx.writeFileSync(subfolder + '/' + subfilename, fsx.readFileSync(subfolder + '/' + subfilename).toString()
                             .replace(/^\s*example: \[\]\s*$/gm, "") // Remove example field from yml as the OPS schema does not support it.
-                            .replace(/description: \\\*\n/g, "description:") // Remove descriptions that are just "\*".
+                            .replace(/description: \\\*[\r\n]/g, "description:") // Remove descriptions that are just "\*".
                             .replace(/\\\*/gm, "*")); // Fix asterisk protection.
                     }
                 });
